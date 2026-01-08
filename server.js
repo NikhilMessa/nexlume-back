485c4467042261a9a077fb0048f66ced5709115f
Nex-Lume <nexlume.co@gmail.com>
2026-01-08 17:56:10 +0530

    Force Brevo API, remove SMTP completely

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.use("/api/team", teamRoutes);
 // =====================
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
